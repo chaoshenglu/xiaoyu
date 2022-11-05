348bf583515642cdec239c8aa4025d146e0a29c8
lixiang <271896700@qq.com>
2022-11-05 10:13:50 +0800

    1

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -9,21 +9,21 @@ import 'screens/navigator.dart';
 
 class Config {
   /// 主题颜色
-  static const int _primaryColorValue = 0xFF166EC9;
+  static const int _primaryColorValue = 0xff00c498;
   static const Color primaryColor = Color(_primaryColorValue);
   static const MaterialColor primarySwatchColor = MaterialColor(
     _primaryColorValue,
     <int, Color>{
-      50: Color(0xFFD1E3F6),
-      100: Color(0xFFA7C9ED),
-      200: Color(0xFF7EB0E4),
-      300: Color(0xFF5999DB),
-      400: Color(0xFF3683D2),
+      50: Color(0xFFadff2f),
+      100: Color(0xFF7fff00),
+      200: Color(0xFF7cfc00),
+      300: Color(0xFF00ff00),
+      400: Color(0xFF00fa9a),
       500: Color(_primaryColorValue),
-      600: Color(0xFF1258A1),
-      700: Color(0xFF0d4279),
-      800: Color(0xFF092C50),
-      900: Color(0xFF041628),
+      600: Color(0xFF228b22),
+      700: Color(0xFF008000),
+      800: Color(0xFF006400),
+      900: Color(0xff156148),
     },
   );
 
